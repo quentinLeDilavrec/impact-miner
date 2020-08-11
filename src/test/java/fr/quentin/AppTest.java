4bc5764748245e3d2c280c2c49878dd5d4049041
quentin le dilavrec <quentin.le-dilavrec@irisa.fr>
2020-08-11 16:18:22 +0200

    exc

--- src/test/java/fr/quentin/AppTest.java
+++ src/test/java/fr/quentin/AppTest.java
@@ -15,7 +15,7 @@ import fr.quentin.impactMiner.AugmentedAST;
 import fr.quentin.impactMiner.ImpactAnalysis;
 import fr.quentin.impactMiner.ImpactChain;
 import fr.quentin.impactMiner.Position;
-import fr.quentin.impactMiner.ImpactAnalysis.ImpactAnalizerException;
+import fr.quentin.impactMiner.ImpactAnalysis.ImpactAnalysisException;
 import spoon.MavenLauncher;
 import spoon.support.compiler.jdt.JDTBasedSpoonCompiler;
 
@@ -32,7 +32,7 @@ public class AppTest {
     }
 
     @Test
-    public void simpleSysTest() throws ImpactAnalizerException {
+    public void simpleSysTest() throws ImpactAnalysisException {
         MavenLauncher launcherAll = new MavenLauncher(
                 "/home/quentin/resources/Versions/graphhopper/graphhopper/7f80425b6a0af9bdfef12c8a873676e39e0a04a6/",
                 MavenLauncher.SOURCE_TYPE.ALL_SOURCE);
