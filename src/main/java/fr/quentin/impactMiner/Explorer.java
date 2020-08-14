c6fae03a781b4e82fc0dfe6fcb7ff9f6a55d06dc
quentin le dilavrec <quentin.le-dilavrec@irisa.fr>
2020-08-14 11:31:56 +0200

    clean

--- src/main/java/fr/quentin/impactMiner/Explorer.java
+++ src/main/java/fr/quentin/impactMiner/Explorer.java
@@ -244,7 +244,6 @@ public class Explorer {
             }
             Set<ImpactChain> extendeds = followUsage(current, (CtExecutable<?>) current_elem, weight);
             callChains.addAll(extendeds);
-            return ImpactType.Level.CALL_GRAPH;
         } else if (current_elem instanceof CtAbstractInvocation) {
             CtExecutable<?> parentExe = getHigherExecutable(current_elem);
             ImpactChain extended = current.extend(getImpactElement(parentExe), ImpactType.EXPAND, weightedMore(0));
